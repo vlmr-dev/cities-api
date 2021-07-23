233e9b0d11c1b523e10fa73218b3246c2ff24d34
vlmr-dev <vlmr.info@gmail.com>
2021-07-23 11:13:47 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cities API
+
+## Requisitos
+
+* Java 11
+* Docker -> Postgres
+* Git
+* IntelliJ Community
+* Heroku
---
+++ src/test/java/com/github/vlmr/dev/cities/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vlmr.dev.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
