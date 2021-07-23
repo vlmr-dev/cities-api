d5bc20003941bd05a011b31aac7c1cfdbe848e57
vlmr-dev <vlmr.info@gmail.com>
2021-07-23 12:26:00 -0300

    Adiciona o pacote: countries, o pacote: entities e a class: Country

---
+++ src/main/java/com/github/vlmr/dev/cities/countries/entities/Country.java
@@ -0,0 +1,47 @@
+package com.github.vlmr.dev.cities.countries.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pais")
+public class Country {
+
+    @Id
+    private Long Id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "nome_pt")
+    private String portugueseName;
+
+    @Column(name = "sigla")
+    private String code;
+
+    private Integer bacen;
+
+    public Country() { }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPortugueseName() {
+        return portugueseName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Integer getBacen() {
+        return bacen;
+    }
+}
