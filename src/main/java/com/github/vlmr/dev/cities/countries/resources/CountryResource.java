971d943205c8b4746cd3d9f73765b7628953c9c7
vlmr-dev <vlmr.info@gmail.com>
2021-07-23 12:39:37 -0300

    Adiciona o pacote: resources e a classe: CountryResource

---
+++ src/main/java/com/github/vlmr/dev/cities/countries/resources/CountryResource.java
@@ -0,0 +1,36 @@
+package com.github.vlmr.dev.cities.countries.resources;
+
+import com.github.vlmr.dev.cities.countries.entities.Country;
+import com.github.vlmr.dev.cities.countries.repositories.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryResource {
+
+    private CountryRepository repository;
+
+    public CountryResource(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Page<Country> Countries(Pageable page){ return repository.findAll(page); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){return ResponseEntity.ok().body(optional.get());}
+        else {return ResponseEntity.notFound().build();}
+
+    }
+
+}
